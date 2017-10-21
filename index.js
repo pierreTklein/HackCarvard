6c9a4e8d2302c6cd58d79c055f7f7a2fcec339b0
pierreTklein <pierre.klein@mail.mcgill.ca>
2017-10-21 15:36:55 -0400

    Added dependency to body-parser

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+
 const app = express()
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -7,4 +12,8 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
+})
+
+app.post('/',function(req, res) {
+
 })
