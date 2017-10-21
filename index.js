748d2e75bcd3694acec646275a614d8e87b4b1d8
Nicholas Lee <niconal902@gmail.com>
2017-10-21 15:38:18 -0400

    testing

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
-
+    console.log(request);
 });
 
 var port = process.env.PORT || 1337;
