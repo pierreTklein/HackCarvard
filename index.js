115d929d45b78258ec9542d65a4fe1d4c80c96eb
Tommy Luo <Tommy_Creatine@Tommys-MacBook-Pro.local>
2017-10-21 19:57:14 -0400

    initial commit

--- index.js
+++ index.js
@@ -18,3 +18,33 @@ app.post('/api/reviews', function(req, res) {
     var make = req.body.make;
     var model = req.body.model;
 });
+
+// res = response
+app.post('/api/getmodel/', function(req, res) {
+
+	var request = require('request');
+	var image = req.body.image;
+
+	var options = {
+	  uri: 'https://dev.sighthoundapi.com/v1/recognition?objectType=vehicle,licenseplate',
+	  method: 'POST',
+	  headers: {
+	  	'Content-type': 'application/json',
+	  	'X-Access-Token': 'B1scq2lizVyZZV1gkBcu3pGvfu3JSAm4HjKS'
+	  },
+	  json: {
+	    "image": image
+	  }
+	};
+
+	request(options, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	json = JSON.parse(response.body);
+		// res.send('API for model of car ' + JSON.stringify(json["objects"]));
+	    res.send('API for model of car ' + JSON.stringify(response.body));
+	  } else {
+	  	res.send('There was an error processing the API call');
+	  }
+	});
+
+})
