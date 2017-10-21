25a33ab47a355b1a240cb43b8bd360f9085774b5
Nicholas Lee <niconal902@gmail.com>
2017-10-21 17:48:37 -0400

    updated to theos version

--- index.js
+++ index.js
@@ -30,13 +30,8 @@ function getPrice(url){
   request(url, function(err, response, body){
 
     const $ = cheerio.load(body)
-<<<<<<< HEAD
-    // console.log('scraping ' + url)
-    console.log($.html())
-    price = $
-=======
     let price = $("span._tA:contains('From')").text();
->>>>>>> 00f70dddce6a127f78bced582d45e23c1bfd89a4
+    console.log(price);
   })
 }
 
