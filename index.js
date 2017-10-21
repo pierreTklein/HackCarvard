f68a6b1e1ce09910ef9533edcdebdb46d088e80d
pierreTklein <pierre.klein@mail.mcgill.ca>
2017-10-21 15:31:28 -0400

    Added hello world to use Express

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var http = require('http');
+const express = require('express')
+const app = express()
 
-var server = http.createServer(function(request, response) {
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
-});
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
